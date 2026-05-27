bc2c6f19eec72da43d72cb3a255f076e086e72ee
Miguel Cliver <miguelcliver@gmail.com>
2026-05-27 08:53:37 -0400

    cambio

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+  import "./layout.css";
+  import favicon from "$lib/assets/favicon.svg";
 
-	let { children } = $props();
+  let { children } = $props();
 </script>
+
+<nav>
+  <div>
+    <p>Noxmusic</p>
+    <p>audio premiun</p>
+  </div>
+  <div>
+    <a href="/">Home</a>
+    <a href="/library">Library</a>
+  </div>
+</nav>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 {@render children()}
