2b468d20b99e91037fa4e52a7a1af0dcb2ff44f2
Miguel Cliver <miguelcliver@gmail.com>
2026-05-31 18:48:20 -0400

    tarjecta

--- src/lib/components/Tarjeta-musica.svelte
+++ src/lib/components/Tarjeta-musica.svelte
@@ -1,7 +1,16 @@
 <script>
-    const {imagen,titulo,artista,album,duracion} =$props() 
+  const { imagen, titulo, artista, album, duracion } = $props();
 </script>
 
+
+  <div>
+    <img src={imagen} alt="" />
+    <p>{titulo}</p>
+    <p>{artista}</p>
+  </div>
+
 <div>
-    
+<p>{album}</p>
 </div>
+
+<div>{duracion}</div>
