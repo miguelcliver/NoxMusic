60825d817e116c1d11e63869e8424cbbbe268428
Miguel Cliver <miguelcliver@gmail.com>
2026-06-11 00:10:43 -0400

    estilos al reproductor de musica

--- src/lib/components/Tarjeta-musica.svelte
+++ src/lib/components/Tarjeta-musica.svelte
@@ -2,6 +2,7 @@
   // @ts-nocheck
 
   import { cambiarCancion } from "$lib/state/cancion-actual.svelte";
+  import Icon from "@iconify/svelte";
 
   const { cancion } = $props();
   const cambiar = () => {
@@ -9,15 +10,19 @@
   };
 </script>
 
-<div class="flex py-3">
-  <div class="flex flex-1 px-3 gap-3">
-    <img src={cancion.album.image} alt="" class="w-20 rounded-2xl" />
-    <p>{cancion.title}</p>
-    <p>{cancion.artist.name}</p>
+<div class="grid grid-cols-12 py-4 border border-purple-500/10  backdrop-blur-md shadow-[0_0_40px_rgba(147,51,234,0.15)] bg-[#181824]/90 gap-5">
+  <div class="col-span-5 flex px-5 gap-4 ">
+    <img src={cancion.album.image} alt="" class="w-20 rounded-2xl  object-cover shadow-md border border-white/5" />
+    <div class="flex flex-col justify-center min-w-0 gap-1">
+    <p class="font-sans text-xl font-black text-white tracking-wide uppercase truncate">{cancion.title}</p>
+    <p class="font-mono text-sm font-medium text-blue-400 tracking-wider truncate">{cancion.artist.name}</p>
+  </div>
   </div>
 
-  <div class="flex-1">
-    <p>{cancion.album.title}</p>
+  <div class="col-span-3 flex items-center justify-center min-w-0 h-full text-center">
+    <p class="font-sans text-sm font-bold text-neutral-400 truncate">{cancion.album.title}</p>
   </div>
-  <button onclick={cambiar}>play</button>
+  <button onclick={cambiar} class="  ">
+    <Icon icon="mdi:play" width="24" height="24" class="" />
+  </button>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,18 +7,21 @@
   let { children } = $props();
 </script>
 
-<nav class="flex relative items-center px-12 py-4">
+<nav class="flex relative items-center px-12 py-4 border border-purple-500/10  backdrop-blur-md shadow-[0_0_30px_rgba(147,51,234,0.08)] bg-[#181824]/80">
   <p
-    class="text-2xl font-bold justify-start bg-linear-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent"
+    class="text-2xl font-bold justify-start bg-linear-to-r from-blue-600 to-cyan-400 bg-clip-text text-transparent"
   >
     NOXMUSIC
   </p>
   <div class="flex gap-10 text-neutral-600 grow justify-center">
-    <a href="/" class="flex items-center gap-1 text-white">
+    <a
+      href="/"
+      class="flex items-center gap-1 text-white font-sans font-bold transition-all duration-300 hover:text-cyan-400"
+    >
       <Icon icon="boxicons:home" width={20} height={20} /> Home
     </a>
 
-    <a href="/library" class="flex items-center gap-1 text-white"
+    <a href="/library" class="flex items-center gap-1 text-white font-sans font-bold transition-all duration-300 hover:text-cyan-400"
       ><Icon
         icon="material-symbols:library-music-outline"
         width={20}
